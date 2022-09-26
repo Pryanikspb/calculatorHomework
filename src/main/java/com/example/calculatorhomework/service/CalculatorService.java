d6c74a4710ac11204707781c9c266b6ab24212e4
Pryanikspb <konstantinprivator@gmail.com>
2022-09-26 15:11:20 +0300

    Homework complete

---
+++ src/main/java/com/example/calculatorhomework/service/CalculatorService.java
@@ -0,0 +1,13 @@
+package com.example.calculatorhomework.service;
+
+public interface CalculatorService {
+
+    public int plus (Integer a, Integer b);
+    public int minus (Integer a, Integer b);
+    public int multiplication (Integer a, Integer b);
+    public double division (Integer a, Integer b);
+
+    public int divisionWithoutRemainder(Integer a, Integer b);
+
+
+}
